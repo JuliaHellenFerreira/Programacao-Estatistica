4cb0802a4167c960c747025211bc1f275dc2e98e
Julia Hellen Ferreira <38631847+JuliaHellenFerreira@users.noreply.github.com>
2019-08-14 22:47:01 -0300

    Add files via upload

---
+++ semana01.R
@@ -0,0 +1,58 @@
+# Semana 1 - Objetos e Classes
+# Exercício 1.1
+
+x<- 0
+y<- -1
+z<- 3/2
+
+# Letra a
+
+a<- x+y+z
+a
+b<- y*z
+a
+c<- z/y
+a
+
+# Letra b
+
+z^2
+z^3
+z^x
+z^y
+
+# Letra c
+
+a^(1/2)
+x^(1/2)
+y^(1/2)
+
+# Letra d
+
+-abs(b)^(1/3)
+(-1/c)^4
+(z^2)^(1/3)
+
+# Letra e
+
+abs(x)
+abs(y)
+abs(z)
+
+#Letra f
+
+exp(x)
+exp(y)
+exp(c)
+
+#Letra g
+
+log(x)
+log(a)
+log(b)
+
+#Letra h
+
+(pi)^(1/2)
+exp(-x)
+
