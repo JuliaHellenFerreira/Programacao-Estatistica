5c848fa6713933a5a8d54609a6999aed7aa37716
Julia Hellen Ferreira <38631847+JuliaHellenFerreira@users.noreply.github.com>
2019-08-20 15:32:51 -0300

    Update semana01.R

--- semana01.R
+++ semana01.R
@@ -92,15 +92,107 @@ b
 c <- 8.3%%3
 c
 
-# Exercício 1.4
+# Exercício 1.3
+# Letra a
+a <- 18%%5
+a
+b = 15%%5
+b
+c <- 8.3%%3
+c
 
-a <- 34
-b <- 33
+# Letra b
 
+a <- 33
+if (a%%2 ==0){
+  print("É par!")
+}else{
+  print("É impar!")
+}
 
-if(b%%2 == 0 ){
-  print("Par")
-}else
-  print("Impar")
+# Letra c 
+
+a <- 3.4
+if(a%%1 != 0){
+  print("É não é um número inteiro!")
+}else{
+  print("É um número inteiro")
+}
+
+# Letra d
+
+a <- -3.4
+if(a%%1 ==0 && a >= 0){
+  print("É um número natural!")
+}else{
+  print("Não é um número natural!")
+}
+
+#Exercício 1.4
+
+a<-seq(1:10)
+b<-seq(1,20,by=2)
+c<-seq(20,1,by=-2) 
+
+# Letra a
+
+x <- b-c
+x
+
+# Letra b
+
+y <- 2*a
+y
+
+#Letra c
+
+z <- a==b
+z
+
+# Letra d
+
+w <- b>c
+w
+
+# Exercício 1.5
+# Letra a
+
+a <- letters
+a
+b <- LETTERS
+b
+
+# Letra b
+
+class(a)
+class(b)
+
+# Letra c
+
+length(a)
+length(b)
+
+# Letra d
+
+a <- c(LETTERS,letters)
+a
+
+# Letra e
+
+b <- paste(LETTERS,letters)
+b
+
+# Exercício 1.6
+
+# Letra a
+
+m <- matrix(c(1,2,3,4,101,102,103,104),nrow = 4,ncol = 2, byrow = F)
+m
+
+# Letra b
+
+m <- matrix(c(1,2,3,4,101,102,103,104),nrow = 2,ncol = 4, byrow = T)
+m
+
 
 
