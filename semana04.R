0fae8976ddbab683e1f97e4cfa8c8f69a3d8e877
Julia Hellen Ferreira <38631847+JuliaHellenFerreira@users.noreply.github.com>
2019-09-15 12:13:40 -0300

    Add files via upload

--- semana04.R
+++ semana04.R
@@ -87,7 +87,7 @@ mediana <- function(v){
   v1 <- sort(v)
   n <- length(v1)
   if(n %% 2 == 0){
-    return(v1[n/2] + v1[(n/2) + 1])
+    return((v1[n/2] + v1[(n/2) + 1]) / 2)
   }else
     return(v1[(n + 1)/2])
 }
@@ -103,7 +103,7 @@ quartil <- function(v){
   n <- length(v)
   if( n %% 2 != 0){
     k <- ((n + 1) / 2) - 1
-    s <- ((n + 1) / 2) -1
+    s <- ((n + 1) / 2) + 1
   }else{
     k <- n / 2
     s <- (n / 2) + 1
@@ -135,33 +135,123 @@ met2 <- function(v){
   q1 <- mediana(v1)
   q2 <- mediana(v)
   q3 <- mediana(v2)
-return(c(q1,q2,q3))
+  return(c(q1,q2,q3))
 }
 
 met2(c(1,2,3,4,5,6))
 
-# Exercício 4.6
+# Exercício 4.6 ##### Não está funcionando ####
 
-# Letra a ## Não está funcionando ##
+# Letra a 
 
-frequencia <- function(v){
-  valores <- v[1]
-  freq <- 1
+freqabsoluta <- function(v){
   n <- length(v)
+  val <- NULL
+  freq <- NULL
+  val[1] <- v[1]
+  freq[1] <- 1
   for(i in 2 : n){
-    pertence <- F
-    m <- length(valores)
-    for(i in 1: m){
-      if( valores[i] == v[i]){
-        freq[i] <- freq[j] + 1
-        pertence <- T
-      }
-      if( pertence == F){
-        valores <- c(valores, v[i])
-        freq <- c(freq, 1)
-      }
-      }
-      }
+    if(v[i] == val[k]){
+      freq[k] <- freq + 1
+    }else{
+      j <- length(val)
+      val[j + 1] <- v[i]
+      freq[j + 1] <- 1
+    }
+    M <-matrix(0, nrow = 2, ncol = length(freq))
+    M[1,] <- val
+    M[2,] <- freq
   }
-  return(valores)
+  return(M)
 }
+
+freqabsoluta(c(2,3,3,4,4,4,5))
+
+# Exercício 4.7 ##### Não sei ####
+
+# Exercício 4.8
+
+# Letra a ## No caderno ##
+
+# Letra b
+
+maximo <- function(v){
+  n <- length(v)
+  maior <- v[1]
+  for(i in 2: n){
+    if( maior < v[i]){
+      maior <- v[i]
+    }
+  }
+  return(maior)
+}
+
+maximo(c(2,3,1,3,7))
+
+minimo <- function(v){
+  n <- length(v)
+  menor <- v[1]
+  for(i in 1: n){
+  if( menor > v[i]){
+    menor <- v[i]
+  }
+  }
+  return(menor)
+}
+
+minimo(c(2,3,1,3,7))
+
+amplitudetotal <- function(v){
+  max <- maximo(v)
+  min <- minimo(v)
+  at <- max - min
+  return(at)
+}
+
+amplitudetotal(c(3,4,5,74,2,4,5))
+
+# Exercício 4.9
+
+# Letra a ## No caderno ##
+
+# Letra b
+
+mediana1 <- function(v){
+  v <- sort(v)
+  n <- length(v)
+  if(n %% 2 == 0){
+    return((v[n/2] + v[(n/2) + 1])/ 2)
+  }else{
+    return(v[(n + 1)/2])
+  }
+}
+mediana1(c(1,2,3,4))
+
+
+quartil1 <- function(v){
+  m <- mediana1(v)
+  v <- sort(v)
+  n <- length(v)
+  if(n %% 2 != 0){
+    k <- ((n + 1) / 2) - 1
+    s <- ((n + 1) / 2) + 1
+  }else{
+    k <- n / 2
+    s <- (n / 2) + 1
+  }
+  w <- v[1:k]
+  u <- v[s:n]
+  q1 <- mediana1(w)
+  q3 <- mediana1(u)
+  return(c(q1,m,q3))
+}
+
+quartil1(c(1,2,3,4,5,6))
+
+distinter <- function(v){
+  q <- quartil1(v)
+  dis <- q[3] - q[1]
+  return(dis)
+}
+
+distinter(c(1,2,3,4,5,6))
