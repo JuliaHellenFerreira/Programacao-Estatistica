423cbfb17afc2e8a2f51ae457b21b2848346fd66
Julia Hellen Ferreira <38631847+JuliaHellenFerreira@users.noreply.github.com>
2019-10-09 15:32:26 -0300

    Add files via upload

---
+++ semana08.R
@@ -0,0 +1,17 @@
+# Semana 08 - Algoritmos de ordenação
+
+OrdenaRapido <- function(v){
+  
+  n <- length(v)
+  
+  if( n == 1)
+    return(v)
+  
+# Busca do i
+  i <- 2
+  repeat{
+    if( i < n){ #Não achei i, logo o pivo é o maior elemento. Trocar v[1] por v[n].
+      
+    }
+  }
+}
